57a958a2c0d331583bde92c1967538d2836dc1a9
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-06-09 10:57:16 -0400

    more refactoring

--- script.js
+++ script.js
@@ -33,12 +33,12 @@ function resetElement(element) {
 //show score
 function showScores() {
   let percentage = Math.round((score / questions.length) *100);
-  let classPercentage = percentage > 50 ? percentage.className = "good" : percentage.className = "bad" ; 
+  let classPercentage = percentage > 50 ?  "good" : "bad"; 
+  
   let quizEndHTML =
       `
   <h1>Quiz Completed</h1>
-  <h2 id='score'> Your scored: ${score} of ${questions.length}</h2>
-
+  <h2 id='score'> Your score: ${score} of ${questions.length}</h2>
   <h2 class=${classPercentage}> ${percentage}%</h2>
   `;
   quizElement.innerHTML = quizEndHTML;
